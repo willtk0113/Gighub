b58764d815841f4eb8b4c971537b0d86227714f7
Tae <willtk0113@gmail.com>
2019-10-21 14:45:14 +0900

    Improve look and feel of the home page

--- Gighub/Controllers/HomeController.cs
+++ Gighub/Controllers/HomeController.cs
@@ -22,7 +22,8 @@ namespace Gighub.Controllers
             var upcomingGigs = _dbContext.Gigs
                 .Include(g => g.Artist)
                 .Include(g => g.Genre)
-                .Where(g => g.DateTime > DateTime.Now);
+                .Where(g => g.DateTime > DateTime.Now)
+                .ToList();
 
             return View(upcomingGigs);
         }
